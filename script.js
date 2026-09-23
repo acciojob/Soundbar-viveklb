a5486633a41af9a47b04d020f3844c179d0ddc16
vivek bharamshetti <69708672+viveklb@users.noreply.github.com>
2026-09-23 12:36:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const stopButton = document.querySelector(".stop");
+
+let currentAudio = null;
+
+buttons.forEach((button) => {
+  button.addEventListener("click", function () {
+    stopSound();
+
+    const soundName = this.getAttribute("data-sound");
+
+    currentAudio = new Audio(`sounds/${soundName}.mp3`);
+    currentAudio.play();
+  });
+});
+
+stopButton.addEventListener("click", stopSound);
+
+function stopSound() {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+    currentAudio = null;
+  }
+}
